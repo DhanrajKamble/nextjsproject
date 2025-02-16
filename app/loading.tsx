3cd350dd67be0fa16024266fbd28ebeecdfba075
Dhanraj_Kamble <dhanrajkamble3518@gmail.com>
2025-02-16 10:01:59 +0530

    Loading UI

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+// I created this file on root folder, this means before rendering all things, it will render first
+const Loading = () => {
+  return (
+    <div>___________________</div>
+  )
+}
+
+export default Loading
